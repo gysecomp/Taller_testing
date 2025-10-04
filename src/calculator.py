428f48693e5d20747943b7e904500758c65f8d79
gysecomp <gysecomp@gmail.com>
2025-10-04 09:55:54 -0500

    Prueba de regression

--- src/calculator.py
+++ src/calculator.py
@@ -9,3 +9,11 @@ def division(a,b):
         return "Error: División por cero no está definida."
     return a/b
 
+def division2(a,b):
+    if b == 0:
+        return "Error: División por cero no está definida."
+    return round(a/b)
+
+
+print(division(5,2))
+print(division2(5,2))
--- tests/test_unit_calculator.py
+++ tests/test_unit_calculator.py
@@ -1,6 +1,6 @@
 #test unitario de la suma, resta y división
 
-from src.calculator import suma, resta, division
+from src.calculator import suma, resta, division, division2
 
 def test_suma():
     assert suma(2, 3) == 5
@@ -12,3 +12,6 @@ def test_resta():
 
 def test_division():
     assert division(10, 2) == 5
+
+def test_division2():
+    assert division2(10, 2) == 5
